ec6cfcbef3e4dee12f4aadf7633642179cdf0479
Anton Holovchenko <golvcenkoanton@gmail.com>
2024-08-24 17:33:03 +0200

    Add deployment scripts for tokens and CFMM

---
+++ deploy/00-deploy-tokens.js
@@ -0,0 +1,35 @@
+const { network } = require("hardhat")
+const { devNetworks, getNetworkConfig } = require("../utils/helpers/helper-hardhat")
+const { verify, tenderlyVerify } = require("../utils/contract-verification/verify")
+
+module.exports = async({deployments, getNamedAccounts}) => {
+    const {deploy, log} = deployments
+    const {deployer} = await getNamedAccounts()
+    const contractsNames = ["TokenA", "TokenB"]
+    const networkName = network.name
+    const config = await getNetworkConfig(networkName)
+
+    for (let i = 0; i < contractsNames.length; i++) {
+        const contractName = contractsNames[i]
+        log(`\n============ Deploying ${contractName} contract to ${networkName} network ============\n`)
+        
+        const contract = await deploy(contractName, {
+            from: deployer,
+            log: true,
+            args: [],
+            blockConfirmations: config.blockConfirmations
+        })
+    
+        log(`\n============ Contract deployed to: ${contract.address}  ============\n`)
+        
+        if (!devNetworks.includes(networkName) && process.env.ETHERSCAN_API_KEY) {
+            networkName == "sepolia"
+            ? await verify(contract.address, contract.args)
+            : await tenderlyVerify(contractName, contract.address)
+        } else {
+            log(`You are on the local network, no verification required!\n`)
+        }
+    }
+}
+
+module.exports.tags = ["token", "all"]
